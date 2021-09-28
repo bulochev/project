d5acf1deed767c0d6d13ef4bdaddf730d41c2cd7
Oleg <bulochev.og@outlook.com>
2021-09-28 20:32:59 +0300

    done all 3 tasks of course:)

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-alert("What the **** r u doin here??")
-console.log("r u kidding me?")
+let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+//alert(numberOfFilms);
+
+let obj = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+//alert(obj.count);
+let lastFilm1 = prompt("Какой последний фильм вы посмотрели?", ''),
+    ratingFilm1 = prompt("Как вы его оцените?", ''),
+    lastFilm2 = prompt("Какой еще последний фильм вы посмотрели?", ''),
+    ratingFilm2 = prompt ("Каквы его оцените?", '');
+
+obj.movies[lastFilm1] = ratingFilm1;
+obj.movies[lastFilm2] = ratingFilm2;
+
+console.log(obj.movies);
+
